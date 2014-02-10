4ddc0c2167fc712af652cedbebcde813ca58c530
ashrafweb <ashrafweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-10 08:21:41 +0000

    new premium version publication
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-prevent-copy-paste-plugin/tags/1.3@854605 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin-core.php
@@ -0,0 +1,198 @@
+<?php
+//define all variables the needed alot
+include 'the_globals.php';
+if($_POST["action"] == 'update')
+{
+	//----------------------------------------------------list the options array values
+	$single_posts_protection = $_POST["single_posts_protection"];
+	$right_click_by_mouse_protection = $_POST["right_click_by_mouse_protection"];
+	$css_protection = $_POST["css_protection"];
+	$home_page_protection = $_POST["home_page_protection"];
+	$show_protection_info = $_POST["show_protection_info"];
+	//----------------------------------------------------Get the  options array values
+	$wpcp_settings = 
+	Array (
+			'single_posts_protection' => $single_posts_protection, // prevent content copy, take 2 parameters
+			'right_click_by_mouse_protection' => $right_click_by_mouse_protection, // Prevent Right Click By Mouse
+			'css_protection' => $css_protection, // PROTECTION BY CSS TECHNIQUES
+			'home_page_protection' => $home_page_protection, // PROTECT THE HOME PAGE OR NOT
+			'show_protection_info' => $show_protection_info // about the plugin
+		);
+		if ($wpcp_settings != '' ) {
+		    update_option( 'wpcp_settings' , $wpcp_settings );
+		} else {
+		    $deprecated = ' ';
+		    $autoload = 'no';
+		    add_option( 'wpcp_settings', $wpcp_settings, $deprecated, $autoload );
+		}
+}else //no update action
+{
+	$wpcp_settings = wpcp_read_options();
+}
+
+?>
+<style>
+#aio_admin_main {
+text-align:left;
+direction:ltr;
+padding:10px;
+margin: 10px;
+background-color: #ffffff;
+border:1px solid #EBDDE2;
+display: relative;
+overflow: auto;
+}
+.inner_block{
+height: 370px;
+display: inline;
+min-width:770px;
+}
+#donate{
+    background-color: #EEFFEE;
+    border: 1px solid #66DD66;
+    border-radius: 10px 10px 10px 10px;
+    height: 58px;
+    padding: 10px;
+    margin: 15px;
+    }
+</style>
+<div id="donate">
+<table border="0" width="100%" cellspacing="0" cellpadding="0" dir="ltr" height="88">
+	<tr>
+		<td align="center"><b>
+<font size="7">
+		<a target="_blank" href="http://www.wp-buy.com/product/wp-content-copy-protection-pro/">Preview</a></font></b></td>
+		<td>Get the premium version and enjoy with a full protection!</td>
+	</tr>
+</table>
+<div style="width: 6px; height: 0px"></div></div>
+<div id="aio_admin_main">
+<form method="POST">
+<input type="hidden" value="update" name="action">
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+	<tr>
+		<td width="77%">
+
+<div class="inner_block">
+	<h2>Smart Wordpress Prevent Copy/Paste Options:</h2>
+	<p>Our plugin protect your content from being copied by any other web sites, 
+	the content is the king and you don't want your content be distributed 
+	without your permission!!</p>
+	<p align="left"><?php echo "<img src='$pluginsurl/images/logo.png' align='center' />";?></p>
+		<table border="0" width="100%" cellspacing="0" cellpadding="0">
+			<tr>
+				<td width="60%">
+		<div><font color="#C47500"><b>Change Options as You Like:</b></font></div>
+	<div style="width: 603px; height: 284px; float: left; border: 1px solid #E9E9E9; padding: 4px" id="layer3">
+		<table border="0" width="100%" height="270" cellspacing="0" cellpadding="0">
+			<tr>
+				<td width="221"><b>Posts</b> Protection</td>
+				<td>
+				<select size="1" name="single_posts_protection">
+				<?php 
+				if ($wpcp_settings['single_posts_protection'] == 'Enabled')
+					{
+						echo '<option selected>Enabled</option>';
+						echo '<option>Disabled</option>';
+					}
+					else
+					{
+						echo '<option>Enabled</option>';
+						echo '<option selected>Disabled</option>';
+					}
+				?>
+				</select>
+				</td>
+				<td width="212">
+				<p align="center"><font color="#008000">For single posts content</font></td>
+			</tr>
+			<tr>
+				<td width="221"><b>Right click </b>(by mouse) Protection</td>
+				<td><select size="1" name="right_click_by_mouse_protection">
+				<?php 
+				if ($wpcp_settings['right_click_by_mouse_protection'] == 'Enabled')
+					{
+						echo '<option selected>Enabled</option>';
+						echo '<option>Disabled</option>';
+					}
+					else
+					{
+						echo '<option>Enabled</option>';
+						echo '<option selected>Disabled</option>';
+					}
+				?>
+				</select></td>
+				<td width="212">
+				<p align="center"><font color="#008000">disallow right mouse 
+				click to prevent saving images</font></td>
+			</tr>
+			<tr>
+				<td width="221">Protection by <b>CSS</b></td>
+				<td>
+				<select size="1" name="css_protection">
+				<?php 
+				if ($wpcp_settings['css_protection'] == 'Enabled')
+					{
+						echo '<option selected>Enabled</option>';
+						echo '<option>Disabled</option>';
+					}
+					else
+					{
+						echo '<option>Enabled</option>';
+						echo '<option selected>Disabled</option>';
+					}
+				?>
+				</select>
+				</td>
+				<td width="212">
+				<p align="center"><font color="#008000">Using CSS special code 
+				to protect the content without JavaScript</font></td>
+			</tr>
+			<tr>
+				<td width="221"><b>Home Page</b> Protection</td>
+				<td>
+				<select size="1" name="home_page_protection">
+				<?php 
+				if ($wpcp_settings['home_page_protection'] == 'Enabled')
+					{
+						echo '<option selected>Enabled</option>';
+						echo '<option>Disabled</option>';
+					}
+					else
+					{
+						echo '<option>Enabled</option>';
+						echo '<option selected>Disabled</option>';
+					}
+				?>
+				</select>
+				</td>
+				<td width="212">
+				<p align="center"><font color="#008000">Dont copy any thing! 
+				even from my homepage</font></td>
+			</tr>
+			<tr>
+				<td width="221">&nbsp;</td>
+				<td>
+				&nbsp;</td>
+				<td width="212">&nbsp;</td>
+			</tr>
+			</table></div>
+
+				<p>&nbsp;</td>
+			</tr>
+	</table>
+	
+	<p align="left">
+				<input type="submit" value="     Save all Settings     " name="B4" style="width: 193; height: 29; border: 1px solid #008000; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px">&nbsp;&nbsp;
+	</p>
+
+	<p>&nbsp;</p>
+</div>
+&nbsp;</td>
+	</tr>
+</table>
+
+<p>
+	</li></p>
+</form></div>
+<p>&nbsp;</p>
